bf2ace46d0f4613c24ce41f1f2318835167c3688
Gungun S <gungun_s@epam.com>
2026-03-02 15:57:32 +0530

    added time endpoint

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
 app.MapGet("/", () => "Multi Branch Pipeline Demo - Main Branch");
+app.MapGet("/time", () => DateTime.Now.ToString());
 
 app.Run();
