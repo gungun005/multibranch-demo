6f40b202228c354be6065e882e2452086fd7ffd4
Gungun S <gungun_s@epam.com>
2026-03-02 16:01:07 +0530

    Added version endpoint

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
 app.MapGet("/", () => "Multi Branch Pipeline Demo - Main Branch");
+app.MapGet("/version", () => "API Version 1.0");
 
 app.Run();
